61b7cbdc94d36000550967e56600b79a0007e961
Diego Salazar <89930+DiegoSalazar@users.noreply.github.com>
2025-03-19 22:23:16 -0400

    aggregate all parts data into a single aggregate

--- src/parts/parts.service.ts
+++ src/parts/parts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { TtiService } from '../supplier-services/tti/tti.service';
 import { MyArrowService } from '../supplier-services/my-arrow/my-arrow.service';
+import { AggregatedPart } from '../interfaces/aggregated-part.interface';
 
 type SupplierService = (TtiService | MyArrowService)[] ;
 
@@ -29,6 +30,20 @@ export class PartsService {
       throw new Error(`No data found for part number ${partNumber}`);
     }
 
-    return parts;
+    const aggregatedPart: AggregatedPart = {
+      name: parts[0].name,
+      description: parts[0].description,
+      totalStock: parts.reduce((agg, p) => agg + p.totalStock, 0),
+      manufacturerLeadTime: Math.min(...parts.map((p) => p.manufacturerLeadTime)),
+      manufacturerName: parts[0].manufacturerName,
+      packaging: parts.reduce((agg, p) => [...agg, ...p.packaging ?? []], []),
+      productDoc: parts[0].productDoc,
+      productUrl: parts[0].productUrl,
+      productImageUrl: parts[0].productImageUrl,
+      specifications: parts[0].specifications,
+      sourceParts: parts[0].sourceParts,
+    }
+
+    return aggregatedPart;
   }
 }
